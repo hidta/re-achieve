5f00196e26401d0da9386b6982ce4be66d817510
hidta <tahkyu3@outlook.jp>
2017-03-28 07:35:55 +0000

    sns

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,38 +2,48 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
          
+  mount_uploader :icon, IconUploader
+
+  has_many :blogs, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
+  
+  def self.find_for_twitter_oauth(auth, sign_in_and_resource = nil)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    unless user
+      user = User.new(
+          name:     auth.info.nickname,
+          image_url: auth.info.image,
+          provider: auth.provider,
+          uid:      auth.uid,
+          email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+          password: Devise.friendly_token[0, 20]
+      )
+      user.skip_confirmation!
+      user.save
+    end
+  user
+  end
+  
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(email: auth.info.email)
     unless user
       user = User.new(
-        name:     auth.extra.raw_info.name,
-        provider: auth.provider,
-        uid:      auth.uid,
-        email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
-        image_url: auth.info.image,
-        password: Devise.friendly_token[0, 20]
-        )
+          name:     auth.extra.raw_info.name,
+          provider: auth.provider,
+          uid:      auth.uid,
+          email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+          image_url:   auth.info.image,
+          password: Devise.friendly_token[0, 20]
+      )
       user.skip_confirmation!
       user.save(validate: false)
-    end
-    user
-  end
-  
-  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
-    user = User.find_by(provider: auth.provider, uid: auth.uid)
-    unless user
-      user = User.new(
-        name:     auth.info.nickname,
-        image_url: auth.info.image,
-        provider:  auth.provider,
-        uid:      auth.uid,
-        email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}`example.com",
-        password: Devise.friendly_token[0, 20]
-        )
-      user.skip_confirmation!
-      user.save
     end
     user
   end
@@ -42,9 +52,6 @@ class User < ActiveRecord::Base
     SecureRandom.uuid
   end
   
-  mount_uploader :icon_url, IconUploader
-  
-  #omniauthでサインアップしたユーザー情報の変更
   def update_with_password(params, *options)
     if provider.blank?
       super
@@ -54,25 +61,18 @@ class User < ActiveRecord::Base
     end
   end
   
-  has_many :blogs
-  has_many :comments
-  
-  has_many :relationships, foreign_key:"follower_id", dependent: :destroy
-  has_many :reverse_relationships, foreign_key:"followed_id", class_name: "Relationship", dependent: :destroy
-  
-  has_many :followed_users, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationships, source: :follower
-  
   def follow!(other_user)
     relationships.create!(followed_id: other_user.id)
   end
-  
+
+#フォローしているかどうかを確認する
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
   end
   
-  def unfollow
+  def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
+  
   
 end
