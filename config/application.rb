401f302ed408b37add8cd17fcf12c52f7cb225b4
hidta <tahkyu3@outlook.jp>
2017-04-02 01:30:12 +0000

    not ganerate unnecessaey files by rails g command

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module Achieve
         controller_specs: true,
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
+      g.javascripts false
+      g.helper false
+      g.test_framework false
     end
   end
 end
